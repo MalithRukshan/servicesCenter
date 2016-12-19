53f17eaded687265391edc7567c8f025f48fd9ad
malitharukshan <malitharukshan1990@gmail.com>
2016-12-19 11:44:44 +0530

    remove unnesasary dependancy from pakage.json

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var app = express();
 
-var socketIO = require("socket.io");
+
 
 // Socket.io
 var io = socketIO();
